f5c93f6535e11d32792afd9bc41af1c6d0719d80
Yifan Zong <yifan.zong@trader.ca>
2021-07-30 22:02:14 -0400

    Added more subplots

--- src/Record.py
+++ src/Record.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file = "left_blink_var1_10"
+file = "left_blink_var1_6"
 input_file = "./NatHacks_Race_Simulator/Collected_Data/RawEEG/left_blink/" + file + "_raw.csv"
 output_file = "./NatHacks_Race_Simulator/Collected_Data/FormattedEEG/left_blink/" + file + "_formatted.csv"
 
@@ -21,12 +21,22 @@ del df["Offset"]
 df.to_csv(output_file)
 
 # Plot formatted data
-fig,(graph0, graph1, graph2, graph3, graph4) = plt.subplots(nrows = 5, sharex = True)
+fig,(graph0, graph1, graph2, graph3, graph4, graph5) = plt.subplots(nrows = 6, sharex = True)
 line0, = graph0.plot(df["Time"], df["AF7"], color = "red", linestyle = "solid")
 line1, = graph1.plot(df["Time"], df["AF8"], color = "blue", linestyle = "solid")
 line2, =  graph2.plot(df["Time"], df["TP9"], color = "red", linestyle = "dashdot")
 line3, = graph3.plot(df["Time"], df["TP10"], color = "blue", linestyle = "dashdot")
-graph4.legend((line0, line1, line2, line3), ("AF7", "AF8", "TP9", "TP10"), loc='lower right')
+line4, = graph4.plot(df["Time"], (df["AF7"]/df["AF8"]), color = "green", linestyle = "solid")
+line5, = graph5.plot(df["Time"], (df["TP9"]/df["TP10"]), color = "orange", linestyle = "dashdot")
+
+graph0.title.set_text('AF7')
+graph1.title.set_text('AF8')
+graph2.title.set_text('TP9')
+graph3.title.set_text('TP10')
+graph4.title.set_text('Ratio: AF7 / AF8')
+graph5.title.set_text('Ratio: TP9 / TP10')
+
+#graph4.legend((line0, line1, line2, line3), ("AF7", "AF8", "TP9", "TP10"), loc='lower right')
 plt.xlabel('Time')
 plt.show()
 
