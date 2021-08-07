4bb24d681dfbffdf19bb9fdae6942c99b6d55b2c
Yifan Zong <yifan.zong@trader.ca>
2021-08-07 03:19:30 -0400

    Upgraded optimal models

--- src/Model.py
+++ src/Model.py
@@ -23,19 +23,19 @@ types = ["control", "left_blink", "right_blink"]
 paths = np.array([["left_blink_var1", 1], ["right_blink_var1", 2], ["control_var1", 0]])
 
 # Grid search params
-RandomForest_params = {"xdawncovariances__nfilter": [1, 2, 3, 4, 5],
-                       "randomforestclassifier__n_estimators": [700, 725, 750, 775,  800, 875, 850, 900],
+RandomForest_params = {"xdawncovariances__nfilter": [1, 2, 3, 4, 5, 6, 7, 8],
+                       "randomforestclassifier__n_estimators": [500, 550, 600, 650, 700, 750, 800, 850, 900],
                        "randomforestclassifier__criterion": ["gini", "entropy"],
                        "randomforestclassifier__max_features": [None, "sqrt", "log2"]}
 
-SVC_params = {"xdawncovariances__nfilter": [1, 2, 3, 4, 5],
-              "svc__C": [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35],
-              "svc__gamma": ["scale", 0.01, 0.1, 1, 10, 100] ,
+SVC_params = {"xdawncovariances__nfilter": [1, 2, 3, 4, 5, 6, 7, 8],
+              "svc__C": [0.1, 0.5, 1, 5, 10, 50, 100],
+              "svc__gamma": ["scale", 0.001, 0.01, 0.1, 1, 10, 100] ,
               "svc__kernel": ["rbf"],
-              "svc__decision_function_shape": ["ovo", "ovr"]}
+              "svc__decision_function_shape": ["ovo"]}
 
-MLP_params = {"xdawncovariances__nfilter": [1, 2, 3, 4, 5],
-              "mlpclassifier__hidden_layer_sizes": [(180,), (190,), (200,), (210,), (220), (230), (240,)],
+MLP_params = {"xdawncovariances__nfilter": [1, 2, 3, 4, 5, 6, 7, 8],
+              "mlpclassifier__hidden_layer_sizes": [(180,), (190,), (200,), (210,), (220), (230)],
               "mlpclassifier__activation": ["relu", "tanh", "logistic"],
               "mlpclassifier__solver": ["adam", "sgd", "lbfgs"],
               "mlpclassifier__learning_rate": ["constant", "invscaling", "adaptive"],
@@ -69,10 +69,9 @@ def read_csvs(folder, types, paths):
     return output_x, output_y
 
 # Train model
-def train_model(X, y, model, cv, params, file):
+def train_model(X, y, model, cv, params):
     optimized_model = GridSearchCV(model, params, scoring= "balanced_accuracy", n_jobs = -1, refit = True, cv = cv)
     optimized_model.fit(X, y)
-    dump(optimized_model, './models/' + file)
     print(optimized_model.best_params_)
     return optimized_model
 
@@ -90,17 +89,29 @@ X, y = read_csvs(folder, types, paths)
 print("Finished reading")
 cv = KFold(10, shuffle=True)
 
+"""
 # Setup piplines
 randomForest_Pipepline = make_pipeline(XdawnCovariances(nfilter = 4, estimator = "oas"), TangentSpace(metric = "riemann"), RandomForestClassifier())
 sVC_Pipepline = make_pipeline(XdawnCovariances(nfilter = 4, estimator = "oas"), TangentSpace(metric = "riemann"), SVC())
 mLP_Pipepline = make_pipeline(XdawnCovariances(nfilter = 4, estimator = "oas"), TangentSpace(metric = "riemann"), MLPClassifier(max_iter = 1000))
 
 # Find optimal params
-optimal_RandomForest = train_model(X, y, randomForest_Pipepline, cv, RandomForest_params, "RandomForest.joblib")
-optimal_SVC = train_model(X, y, sVC_Pipepline, cv, SVC_params, "SVC.joblib")
-optimal_MLP = train_model(X, y, mLP_Pipepline, cv, MLP_params, "MLP.joblib")
+optimal_RandomForest = train_model(X, y, randomForest_Pipepline, cv, RandomForest_params)
+optimal_SVC = train_model(X, y, sVC_Pipepline, cv, SVC_params)
+optimal_MLP = train_model(X, y, mLP_Pipepline, cv, MLP_params)
+"""
+
+# The best params are found and new pipeline with those params are created
+optimal_RandomForest_Pipeline = make_pipeline(XdawnCovariances(nfilter = 5, estimator = "oas"), TangentSpace(metric = "riemann"), RandomForestClassifier(criterion = "gini", max_features = "sqrt", n_estimators = 725))
+optimal_SVC_Pipeline = make_pipeline(XdawnCovariances(nfilter = 3, estimator = "oas"), TangentSpace(metric = "riemann"), SVC(C = 30, decision_function_shape = "ovo", gamma = 0.005, kernel = "rbf",))
+optimal_MLP_Pipeline = make_pipeline(XdawnCovariances(nfilter = 5, estimator = "oas"), TangentSpace(metric = "riemann"), MLPClassifier(activation = "relu", hidden_layer_sizes = (210,), learning_rate = "adaptive", solver = "sgd", max_iter = 5000))
 
 # Evaluate models
-evaluate_model(X, y, randomForest_Pipepline, cv)
-evaluate_model(X, y, sVC_Pipepline, cv)
-evaluate_model(X, y, mLP_Pipepline, cv)
+evaluate_model(X, y, optimal_RandomForest_Pipeline, cv)
+evaluate_model(X, y, optimal_SVC_Pipeline, cv)
+evaluate_model(X, y, optimal_MLP_Pipeline, cv)
+
+# Dump trained models
+dump(optimal_RandomForest_Pipeline.fit(X, y), './models/' + "RandomForest.joblib")
+dump(optimal_SVC_Pipeline.fit(X, y), './models/' + "SVC.joblib")
+dump(optimal_MLP_Pipeline.fit(X, y), './models/' + "MLP.joblib")
